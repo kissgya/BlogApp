d47fbdb67e7ec8534ab9780843be6a1017d38497
kissgya <kissgy@kkszki.hu>
2026-09-22 17:53:57 +0200

    Add project files.

---
+++ BlogApi/Controllers/BloggerController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using BlogApi.Models;
+using BlogApi.Models.DTOs;
+
+namespace BlogApi.Controllers
+{
+    [Route("blogger")]
+    [ApiController]
+    public class BloggerController : ControllerBase
+    {
+        public string ConnectionString = "server=localhost;database=blog;uid=root;password=;";
+
+        [HttpGet]
+        public object GetAllBlogger()
+        {
+            List<Blogger> bloggers = new List<Blogger>();
+            var connector = new MySqlConnection(ConnectionString);
+
+            connector.Open();
+
+            string sql = "SELECT * FROM blogger;";
+
+            var cmd = new MySqlCommand(sql, connector);
+
+            var datareader = cmd.ExecuteReader();
+
+            while (datareader.Read())
+            {
+                var blogger = new Blogger
+                {
+                    Id = datareader.GetInt32(0),
+                    Name = datareader.GetString(1),
+                    Email = datareader.GetString(2),
+                    Age = datareader.GetInt32(3),
+                    Password = datareader.GetString(4),
+                    RegistrationTime = datareader.GetDateTime(5),
+                };
+
+                bloggers.Add(blogger);
+            }
+
+            connector.Close();
+
+            return new {message = "Sikeres lekérdezés", result = bloggers};
+        }
+
+        [HttpGet("count")]
+        public object GetBloggerCount()
+        {
+            var connector = new MySqlConnection(ConnectionString);
+            connector.Open();
+
+            string sql = "SELECT COUNT(*) FROM blogger;";
+            var cmd = new MySqlCommand(sql, connector);
+
+            var count = Convert.ToInt32(cmd.ExecuteScalar());
+
+            connector.Close();
+
+            return new { message = "Sikeres lekérdezés", result = count };
+        }
+
+        [HttpGet("contacts")]
+        public object GetOrderedContacts()
+        {
+            var contacts = new List<object>();
+            var connector = new MySqlConnection(ConnectionString);
+
+            connector.Open();
+
+            string sql = "SELECT Name, Email FROM blogger ORDER BY Name ASC;";
+
+            var cmd = new MySqlCommand(sql, connector);
+            var datareader = cmd.ExecuteReader();
+
+            while (datareader.Read())
+            {
+                contacts.Add(new
+                {
+                    Name = datareader.GetString(0),
+                    Email = datareader.GetString(1)
+                });
+            }
+
+            connector.Close();
+
+            return new { message = "Sikeres lekérdezés", result = contacts };
+        }
+
+        [HttpGet("byId")]
+        public object GetBloggerById(int id)
+        {
+            var connector = new MySqlConnection(ConnectionString);
+            
+            connector.Open();
+
+            string sql = @"SELECT * FROM `blogger` WHERE `id` = @id";
+
+            var cmd = new MySqlCommand(sql, connector);
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            var datareader = cmd.ExecuteReader();
+
+            Blogger blogger = null;
+            object result = null;
+
+            if (datareader.Read() == true)
+            {
+                blogger = new Blogger
+                {
+                    Id = datareader.GetInt32("id"),
+                    Name = datareader.GetString("name"),
+                    Email = datareader.GetString("email"),
+                    Age = datareader.GetInt32("age"),
+                    Password = datareader.GetString("password"),
+                    RegistrationTime = datareader.GetDateTime("registrationTime")
+                };
+
+                result = new { message = "Sikeres lekérdezés", result = blogger };
+            }
+            else
+            {
+                result = new { message = "Nincs ilyen Id.", result = blogger }; 
+            }
+            
+            connector.Close();
+            return result;            
+        }
+
+        [HttpPost]
+
+        public object AddNewBlogger(AddNewBloggerDto addNewBloggerDto)
+        {
+
+            return new { message = "Sikeres felvétel.", result = "" };
+        }
+    }
+}
---
+++ BlogApi/Models/Blogger.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApi.Models
+{
+    public class Blogger
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Password { get; set; } = string.Empty;
+        public DateTime RegistrationTime { get; set; }
+    }
+}
