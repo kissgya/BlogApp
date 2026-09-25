bbb754d0ae19015fce4f5c0d7828f706101ae4fd
kissgya <kissgy@kkszki.hu>
2026-09-25 12:25:18 +0200

    2. feladat: Blogpost kontroller letrehozva

---
+++ BlogApi/Controllers/BlogpostController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogpostController : ControllerBase
+    {
+    }
+}
